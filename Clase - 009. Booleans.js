e7d9eebcc3d67b9da29d954f50a6fe6f8c0dded8
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-01-23 19:20:12 -0500

    Agregar archivos de clases para Booleans, Indefinido y actualizar script en Index.html

---
+++ Clase - 009. Booleans.js
@@ -0,0 +1,6 @@
+let verdad = true
+console.log(verdad)
+let mentira = false
+console.log(mentira)
+
+console.log(typeof verdad, typeof mentira)
---
+++ Clase - 010. Indefinido.js
@@ -0,0 +1,7 @@
+let nulo = null
+let indefinido = undefined
+let sinValor;
+
+console.log(nulo);
+console.log(indefinido);
+console.log(sinValor);  
