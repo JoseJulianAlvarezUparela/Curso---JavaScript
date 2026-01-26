722e78ffa8a191cc291ca8c4274d52b7765e99b3
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-01-25 19:18:31 -0500

    Agregar estructura de switch para días de la semana en el archivo de condicionales

--- Clase - 015. Condicionales.js
+++ Clase - 015. Condicionales.js
@@ -24,3 +24,32 @@ if (hora > 0 && hora <= 12) {                               // && = and
 }   else if (hora > 18 && hora <= 24) {
     console.log("Buenas noches")
 }
+
+let dia = 3;
+
+switch (dia) {
+    case 1:
+        console.log("Hoy es lunes");
+        break;
+    case 2:
+        console.log("Hoy es martes");
+        break;
+    case 3:
+        console.log("Hoy es miércoles");
+        break;
+    case 4:
+        console.log("Hoy es jueves");
+        break;
+    case 5:
+        console.log("Hoy es viernes");
+        break;
+    case 6:
+        console.log("Hoy es sábado");
+        break;
+    case 7:
+        console.log("Hoy es domingo");
+        break;
+    default:
+        console.log("Día no válido");
+        break;
+}
