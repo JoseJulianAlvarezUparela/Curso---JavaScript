c54b9deb1bd25cb9c61dfe3e7eb3d12e5944097a
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-02-23 19:48:50 -0500

    Actualizar script en Index.html para usar el nuevo archivo de clase sobre temporizadores y agregar el archivo de clase correspondiente

---
+++ Clase - 035. Temporizadores (setTimeout & setInterval).js
@@ -0,0 +1,8 @@
+console.log("Hola Mundo")
+// setTimeout(() => {
+//     console.log("Hola Mundo después de 3 segundos")
+// }, 3000)
+
+// setInterval(() => {
+//     console.log("Hola Mundo cada 2 segundos")
+// }, 2000)
