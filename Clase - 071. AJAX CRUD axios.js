694e521cc7d2d4fd62d0f59d9f5572602ba6cec1
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-03-23 18:11:13 -0500

    oK

--- Clase - 071. AJAX CRUD axios.js
+++ Clase - 071. AJAX CRUD axios.js
@@ -1,4 +1,5 @@
- const d = document,
+
+const d = document,
       $table = d.querySelector(".crud-table"),
       $form = d.querySelector(".crud-form"),
       $title = d.querySelector(".crud-title"),
