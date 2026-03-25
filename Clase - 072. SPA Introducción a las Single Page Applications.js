b0f04f5b11b339cb9004c1a427381cbbe5998943
Jose Julian Alvarez Uparela <josejulianalvarezu@gmail.com>
2026-03-24 19:21:09 -0500

    SPA iniciado

---
+++ Clase - 072. SPA Introducción a las Single Page Applications.js
@@ -0,0 +1,9 @@
+// Una SPA (Single Page Application) es una aplicación web que carga una sola página HTML al inicio y, a partir de ahí, actualiza el contenido dinámicamente con JavaScript sin recargar toda la página.
+
+// Puntos clave:
+
+// El navegador no “navega” a páginas nuevas completas; solo cambia partes del contenido.
+// Las rutas suelen manejarse con JavaScript (por ejemplo, usando el History API).
+// Ofrece una experiencia más fluida, parecida a una app de escritorio.
+// Requiere más lógica en el front-end y suele comunicarse con APIs para obtener datos.
+// Si quieres, puedo agregar ejem
